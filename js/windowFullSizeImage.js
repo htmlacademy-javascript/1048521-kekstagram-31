cdfa85116521c22d77a7e625c1be41a8152c4248
Kiamova Elvira <ela.kiyamova@yandex.ru>
2024-03-24 00:15:28 +0500

    feat: add the functions of closing the photo with a cross and the ESC key

--- js/windowFullSizeImage.js
+++ js/windowFullSizeImage.js
@@ -5,7 +5,8 @@ const photoInBlockPicture = bigPictureImg.querySelector('img');
 const likesCount = bigPicture.querySelector('.likes-count');
 const commentsCount = bigPicture.querySelector('.social__comment-total-count');
 const socialCaption = bigPicture.querySelector('.social__caption');
-
+const socialCommentCount = bigPicture.querySelector('.social__comment-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
 
 const showingLargePhoto = function(evt) {
   if (evt.target.className === 'picture__img') {
@@ -18,7 +19,28 @@ const showingLargePhoto = function(evt) {
     likesCount.textContent = likes.textContent;
     commentsCount.textContent = comments.textContent;
     socialCaption.textContent = evt.target.alt;
+
+    socialCommentCount.classList.add('hidden');
+    commentsLoader.classList.add('hidden');
+    document.querySelector('body').classList.add('modal-open');
   }
 };
 
 pictures.addEventListener('click', showingLargePhoto);
+
+
+//  закрытие окна большого фото по нажатию на крестик и ESC
+
+const closeButton = bigPicture.querySelector('.big-picture__cancel');
+const closePopupBigPicture = function() {
+  bigPicture.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+};
+closeButton.addEventListener('click', closePopupBigPicture);
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    bigPicture.classList.add('hidden');
+  }
+});
