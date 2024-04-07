8aed8d6b225f7aedf5bb44337a4206c06e875017
Kiamova Elvira <ela.kiyamova@yandex.ru>
2024-04-07 13:50:39 +0500

    feat: add a zoom change to the image

--- js/formUser.js
+++ js/formUser.js
@@ -13,25 +13,13 @@ const closeForm = function() {
   inputIdUploadFile.value = '';
 };
 
-document.addEventListener('keydown', (evt) => {
-  if (evt.key === 'Escape') {
-    if(evt.target === inputHashtag) {
-      return;
-    }
-    if(evt.target === inputDescription) {
-      return;
-    }
-    closeForm();
-  }
-});
-
 const openPhotoEditForm = function() {
   imgUploadOverlay.classList.remove('hidden');
   document.querySelector('body').classList.add('modal-open');
 };
 
 buttonCloseForm.addEventListener('click', closeForm);
-imgUploadLabel.addEventListener('click', openPhotoEditForm);
+imgUploadLabel.addEventListener('change', openPhotoEditForm);
 
 // подлключение валидации с Pristine
 
@@ -92,32 +80,52 @@ formImgUpload.addEventListener('submit', (evt) => {
   pristine.validate();
 });
 
+// изменение масштаба картинки
 
 const btnSmaller = formImgUpload.querySelector('.scale__control--smaller');
 const btnBigger = formImgUpload.querySelector('.scale__control--bigger');
 const inputScaleControlValue = formImgUpload.querySelector('.scale__control--value');
 const resizableImage = formImgUpload.querySelector('.img-upload__preview img');
 
-const step = 0.25;
-let scale = 1;
+const step = 25;
+let scale = 100;
 
 btnSmaller.addEventListener('click', () => {
   if(scale > step) {
-    scale -= step;
-    console.log(scale);
-    resizableImage.style.transform = `scale(${scale})`;
-    inputScaleControlValue.value = `${scale * 100}%`;
+    scale -= 25;
+    resizableImage.style.transform = `scale(${scale / 100})`;
+    inputScaleControlValue.value = `${scale}%`;
   }
 });
 
 btnBigger.addEventListener('click', () => {
-  console.log(scale);
-  if(scale < 1) {
-    scale += step;
-    console.log(scale);
-    resizableImage.style.transform = `scale(${scale})`;
-    inputScaleControlValue.value = `${scale * 100}%`;
+  if(scale < 100) {
+    scale += 25;
+    resizableImage.style.transform = `scale(${scale / 100})`;
+    inputScaleControlValue.value = `${scale}%`;
   }
 });
 
-export {inputDescription, inputHashtag, inputIdUploadFile, imgUploadOverlay};
+// изменение эффекта изображения
+
+const slider = formImgUpload.querySelector('.effect-level__slider');
+const effectLevelValue = formImgUpload.querySelector('.effect-level__value');
+// const inputsEffect = formImgUpload.querySelectorAll('.effects__radio');
+// const imgEffectLevel = formImgUpload.querySelector('.img-upload__effect-level');
+// const effectsList = formImgUpload.querySelector('.effects__list');
+
+noUiSlider.create(slider, {
+  connect: 'lower',
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 100,
+  step: 1
+});
+
+slider.noUiSlider.on('update', () => {
+  effectLevelValue.value = slider.noUiSlider.get();
+});
+
+export {inputDescription, inputHashtag, closeForm};
